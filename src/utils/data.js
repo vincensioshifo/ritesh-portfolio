f4a2bd3b971653c6c6cce50019ff8532fee89c3a
Ritesh Bafna <Riteshbafna025@gmail.com>
2025-12-11 22:16:40 +0530

    data.js fixed

--- src/utils/data.js
+++ src/utils/data.js
@@ -22,13 +22,13 @@ import { FiGithub, FiLinkedin, FiTwitter,
 import { FaXTwitter } from "react-icons/fa6";
 
 //Here import all the assets like images
-import Project1 from '../assets/images/project1.png';
-import Project2 from '../assets/images/project2.png';
-import Project3 from '../assets/images/project3.png';
-import Project4 from '../assets/images/project4.png';
-import Project5 from '../assets/images/project5.png';
-import Project6 from '../assets/images/project6.png';
-import Project7 from '../assets/images/project7.png';
+import Project1 from '../assets/images/Project1.png';
+import Project2 from '../assets/images/Project2.png';
+import Project3 from '../assets/images/Project3.png';
+import Project4 from '../assets/images/Project4.png';
+import Project5 from '../assets/images/Project5.png';
+import Project6 from '../assets/images/Project6.png';
+import Project7 from '../assets/images/Project7.png';
 
 export const SKILLS_CATEGORY = [
     {
